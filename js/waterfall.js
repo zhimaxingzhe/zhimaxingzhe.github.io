efbf035671f86bb57c8371f8d9e72a01504adf4d
Xmas-Shen <ShenPublic@foxmail.com>
2022-11-21 17:08:48 +0800

    * 增加spring boot项目链接

--- js/waterfall.js
+++ js/waterfall.js
@@ -19,6 +19,12 @@
       title: '我的博客',
       core_tech: 'JekyII, JavaScript, HTML, CSS',
       description: '我的博客. 详情见 <a href ="https://shen-xmas.github.io/">这里</a>。'
+    },{
+      demo_link: 'https://github.com/Shen-Xmas/spring-boot-demo',
+      code_link: 'https://github.com/Shen-Xmas/spring-boot-demo',
+      title: 'Java Spring Boot 各种demo',
+      core_tech: 'Java, Spring Boot, Mybatis, Rabbitmq, Dubbo, Redis, Maven, Docker ..',
+      description: '适合初学者入门的一些demo, 个人的一些学习思考, 欢迎指正. 详情见 <a href ="https://github.com/Shen-Xmas/spring-boot-demo">这里</a>。'
     }
   ];
 
